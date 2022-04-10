98458787ed668b644d2341a86d50840abf2df36e
Adam Macumber <adam.macumber@sharpnotions.com>
2022-04-10 19:54:10 -0400

    feat: Replace :colon: syntax with @sign syntax.

--- lua/cmp_vimwiki_tags.lua
+++ lua/cmp_vimwiki_tags.lua
@@ -1,68 +1,85 @@
-local utils = require("utils")
 local source = {}
 
 source.new = function()
-    local self = setmetatable({}, {__index = source})
-    self.cache = {}
-    return self
+  local self = setmetatable({}, { __index = source })
+
+  self.cache = {}
+  self.rg_job = nil
+  self.running_job_id = 0
+  self.timer = vim.loop.new_timer()
+
+  return self
 end
 
 function source.is_available()
-    return vim.bo.filetype == "vimwiki"
+  return vim.bo.filetype == "vimwiki"
 end
 
 function source.get_debug_name()
-    return "vimwiki-tags"
+  return "vimwiki-tags"
 end
 
 function source.get_trigger_characters()
-    return {":"}
+  return { "@" }
 end
 
 function source.get_keyword_pattern()
-    return [=[\%(\s\|^\)\zs:[[:alnum:]_\-\+]*:\?]=]
+  return [[@\(\a\|\d\)*]]
 end
 
-local function get_vimwiki_tags()
-    local file = ".vimwiki_tags"
-    local lines = utils.lines_from(file)
-    local it = {}
-    local used = {}
+function source.complete(self, params, callback)
+  local function on_event(_, data, event)
+    if event == "stdout" then
+      local items = {}
+      local dups = {}
+      -- Remove duplicates
+      for _, v in pairs(data) do
+        -- Remove illegal characters
+        local label = v:gsub(",", "")
+        label = label:gsub("`", "")
 
-    for _, v in pairs(lines) do
-        local tag = vim.fn.matchstr(v, "^\\S\\+")
-        if (not string.match(tag, "^!_.+") and not used[tag]) then
-            table.insert(it, {label = ":" .. tag .. ":"})
-            used[tag] = true
+        if dups[label] == nil then
+          dups[label] = true
+          table.insert(items, { label = label })
         end
+      end
+
+      callback({ items = items, isIncomplete = false })
     end
+  end
 
-    return it
-end
+  self.timer:stop()
+  self.timer:start(
+    -- TODO: Make debounce parameter
+    100,
+    0,
+    vim.schedule_wrap(function()
+      vim.fn.jobstop(self.running_job_id)
+      local rg_options = {
+        "--no-heading",
+        "--no-filename",
+        "--no-line-number",
+        "--only-matching",
+      }
 
-function source.complete(self, _, callback)
-    local bufnr = vim.api.nvim_get_current_buf()
-    local items = {}
-
-    if not self.cache[bufnr] then
-        items = get_vimwiki_tags()
-        if type(items) ~= "table" then
-            return callback()
-        end
-        self.cache[bufnr] = items
-    else
-        items = self.cache[bufnr]
-    end
-
-    callback({items = items or {}, isIncomplete = false})
-end
-
-function source.resolve(completion_item, callback)
-    callback(completion_item)
-end
-
-function source.execute(completion_item, callback)
-    callback(completion_item)
+      local cmd = string.format(
+        "rg %s %s %s",
+        table.concat(rg_options, " "),
+        "'@(\\w|\\d)+'",
+        "~/Seafile/default/Documents/vim-pad"
+      )
+      self.running_job_id = vim.fn.jobstart(
+        cmd,
+        {
+          on_stderr = on_event,
+          on_stdout = on_event,
+          on_exit = on_event,
+          -- TODO: Make CWD parameter
+          cwd = vim.fn.getcwd(),
+        }
+      )
+    end)
+  )
 end
 
 return source
